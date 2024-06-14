b1b42db40249a1a5792191a4c9c018a130e49ddb
PinakGupta <pinakgupta99@gmail.com>
2024-06-14 23:23:04 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+// const express  = require('express');
+// const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());  // to parse the incoming requests with json payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get('/',(req,res)=>{
+//     res.send("Hello World!");
+// })
+
+// console.log("Done");
+app.listen(PORT,()=> {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
