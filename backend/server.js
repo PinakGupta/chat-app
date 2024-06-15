1986511071138337caca6ad5691078b93c7b15ad
PinakGupta <pinakgupta99@gmail.com>
2024-06-15 23:34:34 +0530

    Implemented Socket functionality

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from './db/connectToMongoDB.js';
-// const express  = require('express');
-// const dotenv = require('dotenv');
-const app = express();
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
 dotenv.config();
 
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());  // to parse the incoming requests with json payloads (from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get('/',(req,res)=>{
-//     res.send("Hello World!");
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// console.log("Done");
-app.listen(PORT,()=> {
-    connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`)
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
